cb55052d9e02e6131dccc82eb8e5b8944b96338b
Dmitrij P <118051854+DmitrijP1402@users.noreply.github.com>
2023-06-19 08:34:20 +0200

    Create Receiver.ino

---
+++ Receiver.ino
@@ -0,0 +1,30 @@
+/********************************************
+date:      "19.06.2023"
+author:    "DmitrijP1402 & 	nico0123456"
+filename:  "Receiver.ino"
+********************************************/
+
+void setup() {
+  Serial.begin(9600);  // Start the Serial Monitor
+  
+  pinMode(12, INPUT); // Set pin 12/13 as INPUT
+  pinMode(13, INPUT);
+}
+
+void loop() {
+  bool startbit = digitalRead(13); // Read the state of the start bit
+  byte receivedByte = 0;
+  
+  if (startbit == 1) {
+    startbit = 0;
+    
+    for(int i = 7; i >= 0; i--) { // Receive bits and store them in a byte
+      delay(10);
+      receivedByte |= digitalRead(12) << i;
+      delay(82);
+    }
+    char receivedChar = (char)receivedByte; // Convert the byte to ASCII and print it
+    Serial.print(receivedChar);
+    receivedByte = 0;
+  }
+}
